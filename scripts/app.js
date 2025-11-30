bb9263807a00949abc9edfa8a73e217d081229ff
michal <kmitmichal@gmail.com>
2025-11-30 10:23:21 +0100

    First ver. JavaScript

--- scripts/app.js
+++ scripts/app.js
@@ -5,19 +5,21 @@ console.log(nums);
 
 
 let btnEmail = document.getElementById("btn-access");
+let inputEmail = document.getElementById("access-mail");
 
 function getEmail () {
-    let inputEmail = document.getElementById("access-mail").value;
-    btnEmail.classList.toggle("disabled");
+    let loadEmail = document.getElementById("access-mail").value;
+    inputEmail.classList.toggle("disabled");
 
-    //save to object variable
+    //uložení objektu do proměnné
 
     let formData = {
-        id: 1, //generate random number
+        id: 1, //najděte funkci, která vygeneruje náhodné kladné číslo
         mail: inputEmail,
-        isLocked: false
+        isLocked: false,
+        length: loadEmail.length
     }
-
+    //rest operátor - vložení atributu do objektu, spojení objektů
     let loadData = {...formData, linkProfile: "insert URL here"};
     console.log("new var:" + loadData.linkProfile);
     console.log(loadData);
@@ -27,10 +29,21 @@ function getEmail () {
     }
 }
 
+//načtení vstupu mail
+//vložení již definované funkce v souboru
 btnEmail.addEventListener('click', getEmail);
+inputEmail.addEventListener('mouseover', (e) => {
+    let loadInput = e.target;
+    loadInput.style.background = "red";
+    console.log(loadInput);
+})
 
+//anonymní funkce
 document.addEventListener('click', (e)=> {
     let targetElement = e.target;
     targetElement.style.color = 'black';
     console.log(targetElement);
 })
+
+//vytvořte jinou událost na libovolném elementu webu
+
--- scripts/data.js
+++ scripts/data.js
@@ -1,5 +1,6 @@
 export const nums = [10, 7, 9, 1, 3, 7];
+//spread operator na pole
 export const nextArray = [...nums, 10, 78, 631, 4];
 
 console.log("Merge array:" + nextArray);
-//události
+
